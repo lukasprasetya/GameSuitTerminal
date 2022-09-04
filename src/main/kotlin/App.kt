94c4b95d3e7dc7731bca1dc97b6609a81050da2f
Lukas <lukasprasetya1208@gmail.com>
2022-09-04 12:23:24 +0700

    Finishing feature input player

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -1,4 +1,14 @@
+import model.PlayerDataModel
+import utils.IOUtils
+
 class App {
+
+    private val rock = "ROCK"
+    private val scissor = "SCISSOR"
+    private val paper = "PAPER"
+    private var result = ""
+
+    private val playersData = mutableListOf<PlayerDataModel>()
 
     private fun printHeader() {
         println(
@@ -16,9 +26,21 @@ class App {
         )
     }
 
+    private fun inputPlayer() {
+        for (index in 0..1) {
+            val player = index.plus(1)
+            println("Input Player $player Name: ")
+            val playerName = IOUtils.readString()
+            println("Input Player $player Choice: ")
+            var playerChoice = IOUtils.readString().uppercase()
+
+            playersData.add(PlayerDataModel(playerName, playerChoice))
+        }
+    }
 
     fun run() {
         printHeader()
+        inputPlayer()
     }
 
     companion object {
